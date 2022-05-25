6d5bf892cc1458b47ceac14fd6755a6b4e74a520
siimkas <kasela.siim@gmail.com>
2022-05-25 09:50:20 +0300

    Final fixes
    -Showing genres
    -Providers fix
    -Release date fix
    -Showing the found movie

--- src/back/main.py
+++ src/back/main.py
@@ -16,6 +16,12 @@ app.add_middleware(
 api_key = '15d2ea6d0dc1d476efbca3eba2b9bbfb'
 recommendation_payload = {'api_key': api_key, 'language': 'en-US'}
 
+r = requests.get('https://api.themoviedb.org/3/genre/movie/list',
+                params=recommendation_payload, timeout=10).json()['genres']
+
+movie_genres=dict()
+for genre in r:
+    movie_genres[str(genre['id'])]=genre['name']
 
 @app.get("/recommendation/{title}")
 async def recommendation(title: str = Path(None, description="Title of the basis movie (mandatory)"),
@@ -43,7 +49,8 @@ async def recommendation(title: str = Path(None, description="Title of the basis
     if(r.json()['total_results'] == 0):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No movie found")
 
-    movie_id = r.json()['results'][0]['id']
+    movie_found=r.json()['results'][0]
+    movie_id = movie_found['id']
 
     r = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}/recommendations', params=recommendation_payload, timeout=10)
 
@@ -51,6 +58,7 @@ async def recommendation(title: str = Path(None, description="Title of the basis
         raise HTTPException(status_code=r.status_code, detail="Some error")
 
     results = r.json()['results']
+    results.insert(0,movie_found)
     for movie in results:
         movie['poster_path'] = 'https://image.tmdb.org/t/p/original' + movie['poster_path']
 
@@ -59,10 +67,15 @@ async def recommendation(title: str = Path(None, description="Title of the basis
             params=recommendation_payload,
             timeout=10).json()['imdb_id']
         movie['imdb_path'] = 'https://www.imdb.com/title/' + imdb_id
-
+        id=movie['id']
         providers = requests.get(
-            f'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers?api_key={api_key}',
-            timeout=10).json()['results']['FI']
+            f'https://api.themoviedb.org/3/movie/{id}/watch/providers?api_key={api_key}',
+            timeout=10).json()['results']
+        
+        if 'FI' in providers:
+            providers=providers['FI']
+        else:
+            providers=providers['US']
 
         if 'flatrate' in providers:
             movie['providers'] = providers['flatrate']
@@ -71,5 +84,12 @@ async def recommendation(title: str = Path(None, description="Title of the basis
 
         for provider in movie['providers']:
             provider['logo_path'] = 'https://image.tmdb.org/t/p/original' + provider['logo_path']
+        genres=[]
+        for genre in movie['genre_ids']:
+           genres.append(movie_genres[str(genre)])
+        movie['genre_ids']=genres
+        movie['release_date']=movie['release_date'][0:4]
+    
+   
 
     return results
